cb975794c8863ed4a215d35d539ae1a131377b04
Arunesh Basak <110350616+AruneshBasak@users.noreply.github.com>
2025-04-19 17:26:45 +0530

    Add files via upload

---
+++ Program 4.js
@@ -0,0 +1,12 @@
+let height1 = prompt("Enter the Height: ");
+
+
+let base1 = prompt("Enter the Base: ");
+
+const a = parseInt(height1);
+
+const b = parseInt(base1);
+
+const c = (1/2 * a * b);
+
+console.log(c)
