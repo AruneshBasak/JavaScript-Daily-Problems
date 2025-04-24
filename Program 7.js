9a975cb203e86fd1fb76e802de71fa25d785327f
Arunesh Basak <110350616+AruneshBasak@users.noreply.github.com>
2025-04-24 17:35:13 +0530

    Add files via upload

---
+++ Program 7.js
@@ -0,0 +1,13 @@
+let number = prompt("Enter a number: ");
+console.log("Your number is " + number)
+if (number > 0) {
+    console.log( "The Number is Positive.")
+}
+
+if (number < 0) {
+    console.log( "The Number is Negative ")
+    
+}
+if (number == 0) {
+    console.log("Dont Disturb")
+}
