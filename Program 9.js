ea51a317e2ae5b7685ea51448adfb033723d1ea9
Arunesh Basak <110350616+AruneshBasak@users.noreply.github.com>
2025-04-26 09:31:57 +0530

    Add files via upload

---
+++ Program 9.js
@@ -0,0 +1,18 @@
+
+const number1  = parseInt(prompt('Enter a positive integer: '));
+
+if (number1  < 0) {
+    alert('Error! Factorial for negative number does not exist.');
+}
+
+else if (number1 === 0) {
+    console.log(`The factorial of ${number1} is 1.`);
+}
+
+else {
+    let fact = 1;
+    for (i = 1; i <= number1; i++) {
+        fact *= i;
+    }
+    console.log(`The factorial of ${number1} is ${fact}.`);
+}
