82dc7cedcde489e739a1bef8a2df25b812bac487
Arunesh Basak <110350616+AruneshBasak@users.noreply.github.com>
2025-04-29 21:01:49 +0530

    Add files via upload

---
+++ Program 11.js
@@ -0,0 +1,16 @@
+const number = prompt("Enter a positive integer");
+const numofdigits = number.length;
+let sum = 0;
+let hell = number;
+while (hell > 0) {
+     let remainder = hell % 10;
+    sum += remainder ** numofdigits;
+    hell = parseInt(hell / 10);
+}
+
+if (sum == number) {
+    console.log(`${number} is an Armstrong number`);
+}
+else {
+    console.log(`${number} is not an Armstrong number.`);
+}
