de12ea9f404411dc6dc76eb9f2b9770c6f7e4dd5
Arunesh Basak <110350616+AruneshBasak@users.noreply.github.com>
2025-04-30 21:18:39 +0530

    Add files via upload

---
+++ object_determination.js
@@ -0,0 +1,20 @@
+/*const a = prompt("Enter a object: ")
+const b = prompt("enter a objecrt: ")
+
+
+if (a == b){
+    console.log("True/Same")
+}
+if(a !==b){
+    console.log("False/Not Equal")
+}*/
+
+
+ let name = parseInt(4)
+
+ if (name > 0){
+     name - 1
+}
+console.log (name)
+ 
+ 
