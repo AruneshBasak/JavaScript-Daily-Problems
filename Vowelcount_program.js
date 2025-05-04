2f0ff0d78905949bfd5024ec6ed500388237d524
Arunesh Basak <110350616+AruneshBasak@users.noreply.github.com>
2025-05-04 11:50:04 +0530

    Add files via upload

---
+++ Vowelcount_program.js
@@ -0,0 +1,9 @@
+const RP = "Umbrella"
+let vow = 0
+let con = 0
+for con in RP(con == "A" || con =="E" || con == "I" || con == "O" || con =="U");{
+    
+    console.log(vow += 1)
+
+}
+console.log(vow);
