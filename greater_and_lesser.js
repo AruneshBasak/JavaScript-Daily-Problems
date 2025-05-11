69d02cb52f53edd880d05f274dd330d64c695b7f
Arunesh Basak <110350616+AruneshBasak@users.noreply.github.com>
2025-05-11 20:15:24 +0530

    Add files via upload

---
+++ greater_and_lesser.js
@@ -0,0 +1,9 @@
+const num1 = parseInt(prompt("Enter the number: "))
+const  num2 = parseInt(prompt("Enter the number:  "))
+
+if (num1>=num2){
+    console.log("The large number is ",num1)
+}
+else {
+    console.log("The large number is", num2)
+}
